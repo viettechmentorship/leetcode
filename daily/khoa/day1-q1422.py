1f27a581f71a922b109877d950f5bceac083bf2b
Khoa Le <khoatle165@gmail.com>
2025-01-01 18:04:51 -0500

    Try ruff instead of autopep8

--- daily/khoa/day1-q1422.py
+++ daily/khoa/day1-q1422.py
@@ -2,9 +2,9 @@ class Solution:
   def maxScore(self, s: str) -> int:
     res = count0 = count1 = 0
     for c in s:
-      if c == '1':
+      if c == "1":
         count1 += 1
-    for i in range(len(s)-1):
+    for i in range(len(s) - 1):
       if s[i] == "0":
         count0 += 1
       else:
