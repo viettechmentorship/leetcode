1a801e92d41c8011d66ecede9a819fb57bef1d60
Khoa Le <khoatle165@gmail.com>
2025-01-01 17:45:47 -0500

    Formatting

--- daily/khoa/day1-q1422.py
+++ daily/khoa/day1-q1422.py
@@ -2,9 +2,10 @@ class Solution:
   def maxScore(self, s: str) -> int:
     res = count0 = count1 = 0
     for c in s:
-      if c == '1': count1 += 1
+      if c == '1':
+        count1 += 1
     for i in range(len(s)-1):
-      if s[i] == '0':
+      if s[i] == "0":
         count0 += 1
       else:
         count1 -= 1
