425c5e6588979d323c5456e74ddc30b102402a94
ANHTRAN <tran36a@mtholyoke.edu>
2025-01-08 09:04:24 +0700

    na-daily6

---
+++ daily-2025/ngocanh/day6-3042.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        ans = 0
+        for i in range(len(words) - 1):
+            for j in range(i + 1, len(words)):
+                if words[j].startswith(words[i]) and words[j].endswith(words[i]):
+                    ans += 1
+        return ans
